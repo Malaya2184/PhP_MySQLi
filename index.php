fadf2e2860fef72211609a5e19cd85443aed67c9
Malaya2184 <malayakumarswain8@gmail.com>
2021-08-25 01:29:19 +0530

    basic prfuun added

--- index.php
+++ index.php
@@ -253,6 +253,122 @@
 // echo is_nan(acos(1.01));
 
 // echo round(2.4) . "<br>";
+
+
+//array push
+// $array = array("php", "laravel", "codeigniter");
+ 
+//  //before adding new values 
+//  echo "Before add the value:- ";
+//  print_r($array); echo "<br>";
+ 
+//  //add elements/values in array
+//  array_push($array,"wordpress","bootstrap","html");
+ 
+//  //after adding a new values
+//  echo "After add the value:- ";
+//  print_r($array);
+
+
+//array pop
+// $array = array("php", "laravel", "codeigniter");
+//  //before remove elements array
+//  echo "Before add the value:- ";
+//  print_r($array); echo "<br>";
+ 
+//  //remove elements from array
+//  array_pop($array);
+ 
+//  //after remove elements from array
+//  echo "After add the value:- ";
+ 
+//  print_r($array);
+
+//array search
+// $a=array("a"=>"red","b"=>"green","c"=>"blue");
+// echo array_search("red",$a);
+
+
+//in array
+// $people = array("Peter", "Joe", "Glenn", "Cleveland");
+
+// if (in_array("Glenn", $people))
+//   {
+//   echo "Match found";
+//   }
+// else
+//   {
+//   echo "Match not found";
+//   }
+
+
+//date function
+// date(format, timestamp)
+// echo date("d/m/Y");
+
+// d - The day of the month (from 01 to 31)
+// D - A textual representation of a day (three letters)
+// j - The day of the month without leading zeros (1 to 31)
+// l (lowercase 'L') - A full textual representation of a day
+// N - The ISO-8601 numeric representation of a day (1 for Monday, 7 for Sunday)
+// S - The English ordinal suffix for the day of the month (2 characters st, nd, rd or th. Works well with j)
+// w - A numeric representation of the day (0 for Sunday, 6 for Saturday)
+// z - The day of the year (from 0 through 365)
+// W - The ISO-8601 week number of year (weeks starting on Monday)
+// F - A full textual representation of a month (January through December)
+// m - A numeric representation of a month (from 01 to 12)
+// M - A short textual representation of a month (three letters)
+// n - A numeric representation of a month, without leading zeros (1 to 12)
+// t - The number of days in the given month
+// L - Whether it's a leap year (1 if it is a leap year, 0 otherwise)
+// o - The ISO-8601 year number
+// Y - A four digit representation of a year
+// y - A two digit representation of a year
+// a - Lowercase am or pm
+// A - Uppercase AM or PM
+// B - Swatch Internet time (000 to 999)
+// g - 12-hour format of an hour (1 to 12)
+// G - 24-hour format of an hour (0 to 23)
+// h - 12-hour format of an hour (01 to 12)
+// H - 24-hour format of an hour (00 to 23)
+// i - Minutes with leading zeros (00 to 59)
+// s - Seconds, with leading zeros (00 to 59)
+// u - Microseconds (added in PHP 5.2.2)
+// e - The timezone identifier (Examples: UTC, GMT, Atlantic/Azores)
+// I (capital i) - Whether the date is in daylights savings time (1 if Daylight Savings Time, 0 otherwise)
+
+
+
+
+
+
+
+//random no
+
+// echo(rand() . "<br>");
+// echo(rand() . "<br>");
+
+// echo(rand(10,100));
+
+// echo(getrandmax());
+
+
+// Tip: Use the arsort() function to sort an associative array in descending order, according to the value.
+
+// Tip: Use the ksort(),arsort(), function to sort an associative array in ascending order, according to the key.
+
+// $age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+// asort($age);
+// arr_sort($age);
+// print_r($age);
+
+// sort() - sort arrays in ascending order
+// rsort() - sort arrays in descending order
+// asort() - sort associative arrays in ascending order, according to the value
+// ksort() - sort associative arrays in ascending order, according to the key
+// arsort() - sort associative arrays in descending order, according to the value
+// krsort() - sort associative arrays in descending order, according to the key
+
     ?>
 </body>
 </html>
