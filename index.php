9206ee4a4a4bc3d6d625b39da8aeeefaca2ad53d
Malaya2184 <malayakumarswain8@gmail.com>
2021-08-24 17:24:58 +0530

    new file added

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //// to get php info
+    // phpinfo();
+    ////to add line breaks i.eadd nl2br function then pass the string and add \r\n in between the lines.
+    // . dot is concadination operator in php
+    // echo nl2br("hiii \r\n this is spider");
+    // echo "hiii <br> this is spider";
+
+
+
+
+//Definition and Usage
+// The define() function defines a constant.
+
+// Constants are much like variables, except for the following differences:
+
+// A constant's value cannot be changed after it is set
+// Constant names do not need a leading dollar sign ($)
+// Constants can be accessed regardless of scope
+// Constant values can only be strings and numbers
+
+    ?>
+</body>
+</html>
